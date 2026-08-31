32246d2859ba6a34167263d9980bfd71518776c2
Mohammed Nuhan <nuhanmohammed04@gmail.com>
2026-09-01 00:01:19 +0530

    error clearing

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 // Vercel serverless entry point for the Express API.
-// Vercel serves this catch-all function, which forwards every request to the
-// existing Express application in backend/src/index.js.
-const app = require("../backend/src/index.js");
+// Vercel serves this catch-all function, forwarding every request to the
+// Express application defined in ../index.js.
+const app = require("../index.js");
 
 module.exports = app;
