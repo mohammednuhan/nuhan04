21e9b2c10e3d85e482e3a5424a2f099caa32ac97
Mohammed Nuhan <nuhanmohammed04@gmail.com>
2026-09-16 18:56:15 +0530

    changiin

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
+if ('scrollRestoration' in history) {
+  history.scrollRestoration = 'manual';
+}
+window.scrollTo(0, 0);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
